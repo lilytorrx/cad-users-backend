a9323efea321b8ee38798c180573d2f68625d46f
lilytorrx <109477475+lilytorrx@users.noreply.github.com>
2026-02-23 19:37:22 -0300

    fix: url cors

--- src/main/java/com/api/cadastro/UserController.java
+++ src/main/java/com/api/cadastro/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins="http://localhost:5173")
+@CrossOrigin(origins="https://cad-users-backend-production.up.railway.app/usuarios")
 @RestController
 @RequestMapping("/usuarios")
 public class UserController {
